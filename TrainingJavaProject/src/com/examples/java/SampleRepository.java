6c0923f2be387ad6617c7c26c1a76c5ff27f70a9
svamshikiran <svamshikiran@gmail.com>
2021-03-17 08:03:48 +0530

    Create SampleRepository.java

---
+++ TrainingJavaProject/src/com/examples/java/SampleRepository.java
@@ -0,0 +1,11 @@
+package com.examples.java;
+
+public class SampleRepository {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is a sample class created in the repository");
+
+	}
+
+}
